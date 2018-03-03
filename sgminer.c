3faa064f2c716aed9b0731e540d0109c487cbbf2
Brian Lee <brian112358@gmail.com>
2018-03-02 18:23:26 -0800

    Fixed compile errors in sgminer.c

--- sgminer.c
+++ sgminer.c
@@ -7227,15 +7227,13 @@ static bool checkIfNeedSwitch(struct thr_info *mythr, struct work *work)
         timetravel10_twisted_code(result, work->pool->swork.ntime, code);
     }
     else if (work->pool->algorithm.type == ALGO_X16R) {
-      if (cgpu->algorithm.type == ALGO_X16R) {
     	  if (work->data) {
-          x16r_twisted_code((const uint32_t *)thr->work->data, code);
+        x16r_twisted_code((const uint32_t *)work->data, code);
     	  }
         else {
           strcpy(code, "0123456789ABCDEF");
         }
       }
-    }
 
     if (strcmp(code, mythr->curSequence) == 0) {
         algoSwitch = false;
